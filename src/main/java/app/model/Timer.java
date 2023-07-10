166c45edd693605e65d02800cfbad42e6add490c
710Valera <710aValera@gmail.com>
2023-07-10 12:28:27 +0300

    10.07

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,18 @@
 package app.model;
 
+import org.springframework.context.annotation.Bean;
+
+
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
+    }
+
+    @Bean(name = "beanTimer")
+    public Timer timer() {
+        return new Timer();
     }
 }
